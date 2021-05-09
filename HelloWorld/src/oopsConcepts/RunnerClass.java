8e965d95a39d729cc309a5340c436ebe341b1292
ashwini <ashiashwini19@gmail.com>
2021-05-09 03:52:57 +0000

    CoreJava

---
+++ HelloWorld/src/oopsConcepts/RunnerClass.java
@@ -0,0 +1,17 @@
+package oopsConcepts;
+
+public class RunnerClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ClassA emp1=new ClassA();
+		emp1.Name="Mithali";
+		emp1.OrgName="Organization";
+		emp1.Salary=1200;
+		
+		emp1.Display();
+
+	}
+
+}
