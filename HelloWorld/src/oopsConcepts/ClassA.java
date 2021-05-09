1e9a1f378258502f4920a7f3e1c47d53948f353c
ashwini <ashiashwini19@gmail.com>
2021-05-09 07:22:47 +0000

    Updated Class A

--- HelloWorld/src/oopsConcepts/ClassA.java
+++ HelloWorld/src/oopsConcepts/ClassA.java
@@ -1,11 +1,42 @@
 package oopsConcepts;
 
+import java.util.Scanner;
+
 public class ClassA {
 	
-	int Salary;
-	String Name;
-	String OrgName;
-	public int Sal;
+	 int Salary;
+	 String Name;
+	 String OrgName;
+	 int Sal;
+	 private int value;
+	
+	protected int s1;
+	public ClassA()
+	{
+		System.out.println("Inside the constructor");
+	}
+	
+	/*public ClassA() {
+		
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("enter the salary of the employee : ");
+		int Salary=scanner.nextInt();
+		System.out.println("enter the Name of the employee : ");
+		Scanner scanner1=new Scanner(System.in);
+		String Name=scanner.next();
+		System.out.println("enter the ORganization name of the employee : ");
+		Scanner scanner2=new Scanner(System.in);
+		String OrgName=scanner.next();
+	}*/
+	
+	public  ClassA(int a,String b,String c)
+	{
+		System.out.println("Inside the Parametrized contructor");
+		Salary=a;
+		Name=b;
+		OrgName=c;
+		
+	}
 	
 	public void Display() {
 		System.out.println("Name of the Employee: "+Name);
